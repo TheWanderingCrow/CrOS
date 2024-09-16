91f23ff5ebede827cef9ef687c0cc90a81fd9891
Patrick Menking <contact@wanderingcrow.net>
2024-09-16 14:13:59 -0400

    I understand modules a bit better now

--- modules/core.nix
+++ modules/core.nix
@@ -6,4 +6,27 @@
 }: {
    system.stateVersion = "24.05";
    time.timeZone = "America/New_York";
+
+    options.packages = {
+        enable = lib.mkEnableOption "enables packages";
+        core.enable = lib.mkEnableOption "enables required packages";
+        gui.enable = lib.mkEnableOption "enables gui+DE packages";
+        programming.enable = lib.mkEnableOption "enables programming packages";
+        hacking.enable = lib.mkEnableOption "enables hacking packages";
+        mudding.enable = lib.mkEnableOption "enables mudding packages";
+        gaming.enable = lib.mkEnableOption "enables gaming packages";
+    };
+    
+    config = {
+        packages = {
+            enable = lib.mkDefault true;
+            core.enable = lib.mkDefault true;
+            gui.enable = lib.mkDefault true;
+            programming.enable = lib.mkDefault true;
+            hacking.enable = lib.mkDefault false;
+            mudding.enable = lib.mkDefault false;
+            gaming.enable = lib.mkDefault false;
+        };
+    };
+    
 }
--- modules/default.nix
+++ modules/default.nix
@@ -6,18 +6,4 @@
         ./programs.nix
         ./users
     ];
-
-    options.omni.enable = lib.mkOption {default = true;};
-
-    config = lib.mkIf config.omni.enable {
-        packages = {
-            enable = lib.mkDefault true;
-            core.enable = lib.mkDefault true;
-            gui.enable = lib.mkDefault true;
-            programming.enable = lib.mkDefault true;
-            hacking.enable = lib.mkDefault false;
-            mudding.enable = lib.mkDefault false;
-            gaming.enable = lib.mkDefault false;
-        };
-    };
 }
