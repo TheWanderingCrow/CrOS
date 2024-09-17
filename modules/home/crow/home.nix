8a7c48c8dcc5ae0d886ae16ecd4c7e388025f867
Patrick Menking <contact@wanderingcrow.net>
2024-09-17 16:24:12 -0400

    Autostart waybar

--- modules/home/crow/home.nix
+++ modules/home/crow/home.nix
@@ -15,6 +15,7 @@
     programs = {
         waybar = {
             enable = true;
+            systemd.enable = true;
             settings = {
                 mainBar = {
                     layer = "top";
