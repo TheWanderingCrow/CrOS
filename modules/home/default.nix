9b031294fa1ddf26c51799a731d86f84dbc57b64
Patrick Menking <contact@wanderingcrow.net>
2024-09-17 16:07:19 -0400

    set home-manager backup file extension

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,6 +3,7 @@
     config.home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
+        backupFileExtension = "backup";
         users.crow = lib.mkIf config.users.crow.home.enable ./crow/home.nix;
     };
 }
