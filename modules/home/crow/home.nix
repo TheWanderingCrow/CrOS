6810860b63ef9be95ffc93744688720c83fc8453
Patrick Menking <contact@wanderingcrow.net>
2024-09-17 15:35:11 -0400

    FIx typo

--- modules/home/crow/home.nix
+++ modules/home/crow/home.nix
@@ -11,7 +11,7 @@
                 };
 
                 xdg = {
-                    configHome = "${homeDirectory}/.config";
+                    configHome = "/home/crow/.config";
                     enable = true;
                 };
                 
