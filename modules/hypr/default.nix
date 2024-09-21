4c5728fa1f40c3c47ca121515b12c6c72dc5f861
TheWanderingCrow <patrick@wanderingcrow.net>
2024-09-21 17:02:58 -0400

    x11

--- modules/hypr/default.nix
+++ modules/hypr/default.nix
@@ -1,22 +1,11 @@
 { inputs, pkgs, lib, config, ...}: {
     options.hyprland.enable = lib.mkEnableOption "enables hyprland";
-    options.hypr.enable = lib.mkEnableOption "enables hypr";
 
     config = {
         programs.hyprland = lib.mkIf config.hyprland.enable {
             enable = true;
             package = inputs.hyprland.packages.${pkgs.system}.hyprland;
             portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
-        };
-        
-        services.xserver = lib.mkIf config.hypr.enable {
-            enable = true;
-            windowManager.hypr = {
-                enable = true;
-            };
-            displayManager = {
-                startx.enable = true;
-            };
         };
     };
 
---
+++ modules/i3/default.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+
+  options.i3.enable = lib.mkEnableOption "enables i3wm";
+
+  config = lib.mkIf config.i3.enable {
+
+    services = {
+      libinput = {
+        enable = true;
+        touchpad.naturalScrolling = true;
+      };
+
+      xserver = {
+        enable = true;
+        autoRepeatDelay = 225;
+        autoRepeatInterval = 20;
+
+        windowManager.i3 = {
+          enable = true;
+          configFile = ./i3.config;
+        };
+        displayManager.lightdm.enable = false;
+      };
+    };
+  };
+}
