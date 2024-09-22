2d225e05d1c1f9ac27e64b05fb8227ca4282c8ca
TheWanderingCrow <contact@wanderingcrow.net>
2024-09-22 19:13:44 -0400

    Support for nvidia closed on Parzival

--- hosts/Parzival/default.nix
+++ hosts/Parzival/default.nix
@@ -6,6 +6,10 @@
 
 	networking.hostName = "Parzival";
 
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
+    hardware.nvidia.open = false;
+    hardware.nvidia.modesetting.enable = true;
+
     hyprland.enable = true;
     packages.mudding.enable = true;
     packages.gaming.enable = true;
