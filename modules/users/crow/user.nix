2b034f332126efbfc410c0b8ef2b2706d71032ef
TheWanderingCrow <contact@wanderingcrow.net>
2024-10-03 11:30:33 -0400

    Note backup service

--- modules/users/crow/user.nix
+++ modules/users/crow/user.nix
@@ -5,5 +5,27 @@
         extraGroups = [ "wheel" "networkmanager" "audio" ];
     };
 
+    config.systemd.services.note_sync = {
+        enable = true;
+        description = "Auto sync notes to remote";
+        unitConfig = {
+            User = "crow";
+            Group = "crow";
+            Type = "oneshot";
+        };
+        script = "cd /home/crow/Notes\ngit add .\ngit commit -am 'automatic backup'\ngit push";
+    };
+    config.systemd.timers.note_sync = {
+        enable = true;
+        description = "Timer to autosync notes";
+        timerConfig = {
+            OnCalendar = "*:0/5";
+            Persistent = true;
+        };
+        wantedBy = [
+            timers.target
+        ];
+    };
+
     config.home-manager.users.crow = lib.mkIf config.users.crow.home.enable ./home.nix;
 }
