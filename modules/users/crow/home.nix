d4d1e2ae973a36c08d69b9d45e3bcbfc84caed6c
TheWanderingCrow <contact@wanderingcrow.net>
2024-10-05 16:50:33 -0400

    Fix missing ;

--- modules/users/crow/home.nix
+++ modules/users/crow/home.nix
@@ -67,26 +67,25 @@ in
                     "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
                         installation_mode = "force_installed";
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/{446900e4-71c2-419f-a6a7-df9c091e268b}/latest.xpl";
-                        default_area = "navbar"
+                        default_area = "navbar";
                     };
                     "contact@grimoire.pro" = {
                         installation_mode = "force_installed";
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/contact@grimoire.pro/latest.xpl";
-                        default_area = "navbar"
+                        default_area = "navbar";
                     };
                     "uBlock0@raymondhill.net" = {
                         installation_mode = "force_installed";
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpl";
-                        default_area = "menupanel"
+                        default_area = "menupanel";
                     };
                     # Vimium
                     "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
                         installation_mode = "force_installed",
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/{d7742d87-e61d-4b78-b8a1-b469842139fa}/latest.xpl";
-                        default_area = "menupanel"
+                        default_area = "menupanel";
                     };
                 };
-                        
             };
         };
     };
