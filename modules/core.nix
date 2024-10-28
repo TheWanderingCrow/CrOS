6592150542a5a4a62d1947f86b8c2073255ebaa6
TheWanderingCrow <contact@wanderingcrow.net>
2024-10-28 18:22:09 -0400

    maybe this will fix our portal woes

--- modules/core.nix
+++ modules/core.nix
@@ -80,6 +80,7 @@
             xdgOpenUsePortal = true;
             enable = true;
             wlr.enable = true;
+            lxqt.enable = true;
             extraPortals = [
                 pkgs.xdg-desktop-portal-wlr
                 pkgs.xdg-desktop-portal-gtk
