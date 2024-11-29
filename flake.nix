64bf93b768e5eb4749e229e9e9b64f6e79b3c429
TheWanderingCrow <contact@wanderingcrow.net>
2024-11-29 10:40:10 -0500

    alejandra

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
         hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
         nixvim.url = "git+https://git.wanderingcrow.net/TheWanderingCrow/nvix";
+        alejandra.url = "github:kamadorueda/alejandra/3.1.0";
+        alejandra.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = inputs: let
