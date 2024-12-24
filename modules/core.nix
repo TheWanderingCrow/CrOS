7265b2d4d324fe4467ca8c7177fbd7988620ec76
TheWanderingCrow <patrick@wanderingcrow.net>
2024-12-24 12:23:27 -0500

    repo restructure

--- modules/core.nix
+++ modules/core.nix
@@ -1,19 +1,21 @@
 {pkgs}: {
-  system.stateVersion = "24.05";
-  time.timeZone = "America/New_York";
-  nix.settings.experimental-features = ["flakes" "nix-command"];
-  nix.settings.trusted-users = ["@wheel"];
+  config = {
+    system.stateVersion = "24.05";
+    time.timeZone = "America/New_York";
+    nix.settings.experimental-features = ["flakes" "nix-command"];
+    nix.settings.trusted-users = ["@wheel"];
 
-  environment.variables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-    NIXPKGS_ALLOW_UNFREE = 1;
+    environment.variables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      NIXPKGS_ALLOW_UNFREE = 1;
+    };
+
+    fonts.packages = with pkgs; [
+      font-awesome
+      nerd-fonts.noto
+      nerd-fonts.hack
+      nerd-fonts.jetbrains-mono
+    ];
   };
-
-  fonts.packages = with pkgs; [
-    font-awesome
-    nerd-fonts.noto
-    nerd-fonts.hack
-    nerd-fonts.jetbrains-mono
-  ];
 }
