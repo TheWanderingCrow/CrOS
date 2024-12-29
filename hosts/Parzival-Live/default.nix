16d592cf52e1668b7cc8f6a75bf68e8be023c089
TheWanderingCrow <patrick@wanderingcrow.net>
2024-12-29 11:54:38 -0500

    live build with store optimisation

--- hosts/Parzival-Live/default.nix
+++ hosts/Parzival-Live/default.nix
@@ -10,10 +10,11 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
+  nix.settings.auto-optimise-store = true;
+
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot.supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
-
   networking.wireless.enable = false;
 
   desktop.sway.enable = true;
