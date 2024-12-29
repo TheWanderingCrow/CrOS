f3053f47b0de65b6abce4185109a9a5cc882f682
TheWanderingCrow <patrick@wanderingcrow.net>
2024-12-29 11:50:44 -0500

    live build

--- hosts/Parzival-Live/default.nix
+++ hosts/Parzival-Live/default.nix
@@ -14,7 +14,7 @@
 
   boot.supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
 
-  networking.wireless = false;
+  networking.wireless.enable = false;
 
   desktop.sway.enable = true;
 
