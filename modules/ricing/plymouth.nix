4a0e9831623da50c0519659e74d76762ce060476
TheWanderingCrow <patrick@wanderingcrow.net>
2024-12-30 10:18:55 -0500

    plymouth

--- modules/ricing/plymouth.nix
+++ modules/ricing/plymouth.nix
@@ -17,7 +17,7 @@
     enabledSet = lib.filter (set: config.ricing.${set}.enable) (lib.attrNames rices) // [null];
   in
     if enabledSet != [null]
-    then rices.${enabledSet.head}
+    then lib.head enabledSet
     else null;
 in {
   boot.plymouth = {
