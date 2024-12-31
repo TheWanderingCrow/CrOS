4cae26f26a04d317669639e6546591af5707604f
TheWanderingCrow <patrick@wanderingcrow.net>
2024-12-30 19:30:00 -0500

    as cool as plymouth is, it just doesnt work super well. going to need to find another alternative, perhaps a login manager that supports pre-login animations

--- modules/hardware/boot.nix
+++ modules/hardware/boot.nix
@@ -6,22 +6,7 @@
         efi = {
           canTouchEfiVariables = true;
         };
-        # Can press any key to get the generation list, otherwise just load the first one cause it's probably what you want
-        timeout = 0;
       };
-
-      # Silent Booting
-      consoleLogLevel = 0;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "splash"
-        "boot.shell_on_fail"
-        "loglevel=3"
-        "rd.systemd.show_status=false"
-        "rd.udev.log_level=3"
-        "udev.log_priority=3"
-      ];
     };
   };
 }
--- modules/ricing/default.nix
+++ modules/ricing/default.nix
@@ -1,5 +1,4 @@
 {
   imports = [
-    ./plymouth.nix
   ];
 }
