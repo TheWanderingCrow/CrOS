e01322ec6ace8b328a68d67264ed787c0280f554
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-03 15:09:17 -0500

    add secrets submodule to flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim.url = "git+https://git.wanderingcrow.net/TheWanderingCrow/nvix";
     sops-nix.url = "github:Mic92/sops-nix";
-    nix-secrets.url = "path:./nix-secrets";
+    nix-secrets.url = "git+file://./nix-secrets";
   };
 
   outputs = {
