a723d3404b5b972cde963279d7ce5061d70e03e4
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-05 16:27:18 -0500

    add public key to overseer

--- modules/users/overseer/user.nix
+++ modules/users/overseer/user.nix
@@ -7,5 +7,8 @@
     isNormalUser = true;
     initialPassword = "changeme";
     extraGroups = ["wheel" "libvirtd"];
+    openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCBmjkaAWNBQ6NwiK56miuv30pjheNTZfrULRfPRmed"
+    ];
   };
 }
