2a219611b6d39781332dfdf71184d78725480b14
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-18 12:14:42 -0500

    add zfs support to wce overseer boot params

--- hosts/WCE-Overseer/hardware-configuration.nix
+++ hosts/WCE-Overseer/hardware-configuration.nix
@@ -13,6 +13,7 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+  boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
