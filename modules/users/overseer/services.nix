dbc25f4f865bfa31cc89b49a7fd1ee6f308b704c
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-20 17:14:54 -0500

    needs to be list of paths

--- modules/users/overseer/services.nix
+++ modules/users/overseer/services.nix
@@ -13,7 +13,7 @@ in
       sops = {
         defaultSopsFile = inputs.nix-secrets.secrets.overseer;
         age.keyFile = "/var/lib/sops-nix/key.txt";
-        age.sshKeyPaths = inputs.nix-secrets.keys.overseer;
+        age.sshKeyPaths = [inputs.nix-secrets.keys.overseer];
       };
 
       # Create the dirs we need
