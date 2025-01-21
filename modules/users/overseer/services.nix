ce5de8bd8e8add6efbb7125a7198b8b6d5bfc192
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-21 16:53:31 -0500

    add openmeteo back into config with secrets

--- modules/users/overseer/services.nix
+++ modules/users/overseer/services.nix
@@ -63,8 +63,8 @@ in
           "home.wanderingcrow.net" = {
             locations."/" = {
               extraConfig = ''
-              allow 192.168.0.0/16;
-              deny all;
+                allow 192.168.0.0/16;
+                deny all;
               '';
               proxyPass = "http://localhost:8082";
               proxyWebsockets = true;
@@ -72,6 +72,13 @@ in
           };
         };
       };
+
+        sops.secrets."homepage/openmeteo/lat" = {};
+        sops.secrets."homepage/openmeteo/long" = {};
+        sops.templates."homepage-environment".content = ''
+            HOMEPAGE_VAR_LAT = ${config.sops.placeholder."homepage/openmeteo/lat"}
+            HOMEPAGE_VAR_LONG = ${config.sops.placeholder."homepage/openmeteo/long"}
+        '';
 
       services = {
         homebox = {
@@ -82,6 +89,7 @@ in
         };
         homepage-dashboard = {
           enable = true;
+          environmentFile = config.sops.templates."homepage-environment".path;
           settings = {
             theme = "dark";
           };
@@ -90,6 +98,15 @@ in
               search = {
                 provider = "duckduckgo";
                 target = "_blank";
+              };
+            }
+            {
+              openmeteo = {
+                timezone = "America/New_York";
+                units = "imperial";
+                cache = "5";
+                latitude = "{{HOMEPAGE_VAR_LAT}}";
+                longitude = "{{HOMEPAGE_VAR_LONG}}";
               };
             }
           ];
@@ -109,52 +126,52 @@ in
             {
               "Day to Day" = [
                 {
-                    Messages = [
-                        {
-                            icon = "google-messages.svg";
-                            href = "https://messages.google.com/web";
-                        }
-                    ];
+                  Messages = [
+                    {
+                      icon = "google-messages.svg";
+                      href = "https://messages.google.com/web";
+                    }
+                  ];
                 }
                 {
-                    YouTube = [
-                        {
-                            icon = "youtube.svg";
-                            href = "https://youtube.com";
-                        }
-                    ];
+                  YouTube = [
+                    {
+                      icon = "youtube.svg";
+                      href = "https://youtube.com";
+                    }
+                  ];
                 }
                 {
-                    "Proton Mail" = [
-                        {
-                            icon = "proton-mail.svg";
-                            href = "https://mail.proton.me";
-                        }
-                    ];
+                  "Proton Mail" = [
+                    {
+                      icon = "proton-mail.svg";
+                      href = "https://mail.proton.me";
+                    }
+                  ];
                 }
                 {
-                    Instagram = [
-                        {
-                            icon = "instagram.svg";
-                            href = "https://instagram.com";
-                        }
-                    ];
+                  Instagram = [
+                    {
+                      icon = "instagram.svg";
+                      href = "https://instagram.com";
+                    }
+                  ];
                 }
                 {
-                    Aetolia = [
-                        {
-                            icon = "https://aetolia.com/wp-content/uploads/2020/04/favicon.ico";
-                            href = "https://aetolia.com";
-                        }
-                    ];
+                  Aetolia = [
+                    {
+                      icon = "https://aetolia.com/wp-content/uploads/2020/04/favicon.ico";
+                      href = "https://aetolia.com";
+                    }
+                  ];
                 }
                 {
-                    Amazon = [
-                        {
-                            icon = "amazon.svg";
-                            href = "https://amazon.com";
-                        }
-                    ];
+                  Amazon = [
+                    {
+                      icon = "amazon.svg";
+                      href = "https://amazon.com";
+                    }
+                  ];
                 }
               ];
             }
