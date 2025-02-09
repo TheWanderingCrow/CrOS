b43032257d6dc50782ce02e84235276c1e97e23d
TheWanderingCrow <contact@wanderingcrow.net>
2025-02-09 10:11:12 -0500

    fix relative url

--- modules/users/overseer/services/attic.nix
+++ modules/users/overseer/services/attic.nix
@@ -21,7 +21,7 @@ lib.mkIf config.user.overseer.enable {
       environmentFile = config.sops.templates."attic-env".path;
       settings = {
         listen = "[::]:8080";
-        api-endpoint = "cache.wanderingcrow.net";
+        api-endpoint = "https://cache.wanderingcrow.net/";
         jwt = {};
         chunking = {
           nar-size-threshold = 64 * 1024; # 64 KiB
