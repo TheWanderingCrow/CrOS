c3e1da1dacc53e57114ac44e4efe321251b440e7
TheWanderingCrow <contact@wanderingcrow.net>
2025-03-07 20:50:14 -0500

    user ssh config

--- modules/users/crow/configs/ssh.nix
+++ modules/users/crow/configs/ssh.nix
@@ -5,7 +5,7 @@
         Host github.com
         User git
         PreferredAuthentications publickey
-        IdentityFile /home/crow/.ssh.id_ed25519
+        IdentityFile /home/crow/.ssh/id_ed25519
       Host Overseer
         User overseer
         HostName 192.168.0.30
