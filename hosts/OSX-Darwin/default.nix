a20275a7a4b1d4005104e515543b0376a722330a
TheWanderingCrow <contact@wanderingcrow.net>
2025-03-18 12:24:13 -0400

    flakes and such

--- hosts/OSX-Darwin/default.nix
+++ hosts/OSX-Darwin/default.nix
@@ -1,4 +1,8 @@
 {
   nixpkgs.hostPlatform = "x86_64-darwin";
   system.stateVersion = 6;
+  nix.extraOptions = ''
+    extra-experimental-features = nix-command
+    extra-experimental-features = flakes
+  '';
 }
