46e40c0f3e7056c2ea0727e5c3b9e3d30433b134
TheWanderingCrow <contact@wanderingcrow.net>
2025-04-22 08:38:48 -0400

    rename to infa

--- hosts/OSX-Darwin/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  nixpkgs.hostPlatform = "x86_64-darwin";
-  system.stateVersion = 6;
-  nix.extraOptions = ''
-    extra-experimental-features = nix-command
-    extra-experimental-features = flakes
-  '';
-}
