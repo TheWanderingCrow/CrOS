27af396b1fb235f44c2b2178ff48e47d5b463b33
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-07 09:03:05 -0400

    TODO: finish setting up core user settings

---
+++ home/crow/common/core/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  hostSpec,
+  ...
+}: let
+  platform =
+    if hostSpec.isDarwin
+    then "darwin"
+    else "nixos";
+in {
+  imports = lib.flatten [
+    (map lib.custom.relativeToRoot [
+      "modules/common/host-spec.nix"
+      "modules/home"
+    ])
+    ./${platform.nix}
+    ./xdg.nix
+  ];
+
+  inherit hostSpec;
+
+  home = {
+    username = lib.mkDefault config.hostSpec.username;
+    homeDirectory = lib.mkDefault config.hostSpec.home;
+    stateVersion = lib.mkDefault "24.05";
+
+    sessionVariables = {
+      SHELL = "zsh";
+      TERM = "foot";
+      TERMINAL = "foot";
+      VISUAL = "nvim";
+      EDITOR = "nvim";
+    };
+  };
+}
