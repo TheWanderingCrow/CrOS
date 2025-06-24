8b232acab274248f90e440ef7f5cb5db156dcf12
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-24 11:34:44 -0400

    machines are building woo

---
+++ hosts/nixos/Infiltrator/default.nix
@@ -0,0 +1,49 @@
+############################
+#                          #
+# Infiltrator - Redteam VM #
+#                          #
+############################
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = lib.flatten [
+    # Disks
+    inputs.disko.nixosModules.disko
+    (lib.custom.relativeToRoot "hosts/common/disks/btrfs-disk.nix")
+    {
+      _module.args = {
+        disk = "/dev/vda";
+        withSwap = false;
+      };
+    }
+
+    # Misc
+
+    (map lib.custom.relativeToRoot [
+      # Required configs
+      "hosts/common/core"
+
+      # Optional configs
+    ])
+  ];
+
+  hostSpec = {
+    hostName = "infiltrator";
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  networking = {
+    networkmanager.enable = true;
+    enableIPv6 = false;
+  };
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout = 3;
+  };
+}
