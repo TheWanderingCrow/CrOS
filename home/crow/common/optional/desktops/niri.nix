8d88675778bb12ed239ec5448853c92bea55545d
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-25 16:28:39 -0400

    work on implementing niri

--- home/crow/common/optional/desktops/niri.nix
+++ home/crow/common/optional/desktops/niri.nix
@@ -1,9 +1,19 @@
 {
   pkgs,
+  inputs,
   config,
   ...
 }: {
+  nixpkgs.overlays = [
+    inputs.niri-flake.overlays.niri
+  ];
   home.packages = [
     pkgs.niri
   ];
+
+  programs.niri = {
+    enable = true;
+    settings = {
+    };
+  };
 }
