bfac190b4f5221432f677f4c9be9721143aabb6c
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-30 17:12:32 -0400

    testing sway

--- home/crow/common/optional/desktops/sway/default.nix
+++ home/crow/common/optional/desktops/sway/default.nix
@@ -1,8 +1,27 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
+  home.packages = with pkgs; [
+    foot
+    wofi
+    swaynotificationcenter
+    polkit_gnome
+    swayidle
+    sway-audio-idle-inhibit
+    swaylock-effects
+    sway-contrib.grimshot
+    waybar
+    wl-clipboard
+    hyprlock
+    grim
+    slurp
+    swappy
+    wwljoywake
+  ];
+
   wayland.windowManager.sway = {
     enable = true;
     config = null;
