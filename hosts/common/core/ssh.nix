2a2802303e32c9b6596c080e412d1eb54abcdf5b
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-30 16:28:33 -0400

    test

--- hosts/common/core/ssh.nix
+++ hosts/common/core/ssh.nix
@@ -5,6 +5,18 @@
   pkgs,
   ...
 }: {
+  # Setup access to protected repos via our host ssh key
+  environment.etc."ssh/root_config".text = ''
+    Host github.com
+      User git
+      PreferredAuthentications publickey
+      IdentityFile /etc/ssh/ssh_host_ed25519_key
+  '';
+
+  system.activationScripts.rootSSHConfig.text = ''
+    install -m 0600 -o root -g root /etc/ssh/root_config /root/.ssh/config
+  '';
+
   programs.ssh = {
     startAgent = true;
   };
