718d63e31923e3331bdda498b0e54a41e051c3ea
TheWanderingCrow <contact@wanderingcrow.net>
2025-06-30 17:24:31 -0400

    add fonts

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -1,8 +1,18 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    font-awesome
-    nerd-fonts.noto
-    nerd-fonts.hack
-    nerd-fonts.jetbrains-mono
-  ];
+  fonts = {
+    packages = with pkgs; [
+      font-awesome
+      nerd-fonts.noto
+      nerd-fonts.hack
+      nerd-fonts.jetbrains-mono
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = ["JetBrainsMono Nerd Font" "Hack Nerd Font"];
+        sansSerif = ["Noto Sans"];
+        serif = ["Noto Serif"];
+        emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
 }
