e43fc615b120b0214c18fad80f374d6f49d57723
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-02 15:02:38 -0400

    add handlerone

--- hosts/nixos/HandlerOne/default.nix
+++ hosts/nixos/HandlerOne/default.nix
@@ -3,3 +3,54 @@
 # HandlerOne - m710q #
 #                    #
 ######################
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = lib.flatten [
+    # Hardware
+    ./hardware-configuration.nix
+
+    # Disks
+    inputs.disko.nixosModules.disko
+    (lib.custom.relativeToRoot "hosts/common/disks/btrfs-disk.nix")
+    {
+      _module.args = {
+        disk = "/dev/nvme0n1";
+        withSwap = true;
+        swapSize = "8";
+      };
+    }
+
+    # Misc
+
+    (map lib.custom.relativeToRoot [
+      # Required configs
+      "hosts/common/core"
+
+      # Optional configs
+      "hosts/common/optional/keyd.nix"
+    ])
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  hostSpec = {
+    hostName = "HandlerOne";
+    persistFolder = "/persist";
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    enableIPv6 = false;
+  };
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+    };
+  };
+}
