0707dd7f72f9cc8aaa69f948c767b8ae71006696
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-06 09:22:53 -0400

    that doesnt exist anymore

--- hosts/nixos/HandlerOne/backup.nix
+++ hosts/nixos/HandlerOne/backup.nix
@@ -32,18 +32,17 @@ in
     lib,
     config,
     ...
-  }:
-    lib.mkIf config.user.overseer.enable {
-      # Restic secrets
-      sops.secrets."restic/borg-base/url" = {};
-      sops.secrets."restic/borg-base/key" = {};
+  }: {
+    # Restic secrets
+    sops.secrets."restic/borg-base/url" = {};
+    sops.secrets."restic/borg-base/key" = {};
 
-      services.restic.backups = {
-        borg-base =
-          restic-default
-          // {
-            repositoryFile = config.sops.secrets."restic/borg-base/url".path;
-            passwordFile = config.sops.secrets."restic/borg-base/key".path;
-          };
-      };
-    }
+    services.restic.backups = {
+      borg-base =
+        restic-default
+        // {
+          repositoryFile = config.sops.secrets."restic/borg-base/url".path;
+          passwordFile = config.sops.secrets."restic/borg-base/key".path;
+        };
+    };
+  }
