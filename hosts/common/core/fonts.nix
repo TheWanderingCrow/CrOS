c30b9b9fa030a20b852a23330c3afe11e337ffbb
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-11 10:25:01 -0400

    cover more fonts

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -1,17 +1,47 @@
 {pkgs, ...}: {
   fonts = {
     packages = with pkgs; [
-      font-awesome
-      nerd-fonts.noto
-      nerd-fonts.hack
+      # Emoji and general symbols
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      noto-fonts-extra
+      unifont
+
+      # Nerd fonts
       nerd-fonts.jetbrains-mono
+      nerd-fonts.hack
+      nerd-fonts.fira-code
+      nerd-fonts.noto
+      nerd-fonts.ubuntu-mono
+      nerd-fonts.dejavu-sans-mono
+
+      # Icon fonts
+      font-awesome
+      material-design-icons
     ];
+
     fontconfig = {
       defaultFonts = {
-        monospace = ["JetBrainsMono Nerd Font" "Hack Nerd Font"];
-        sansSerif = ["Noto Sans"];
-        serif = ["Noto Serif"];
-        emoji = ["Noto Color Emoji"];
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "Hack Nerd Font"
+          "FiraCode Nerd Font"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Sans CJK SC"
+          "DejaVu Sans"
+        ];
+        serif = [
+          "Noto Serif"
+          "Noto Serif CJK SC"
+          "DejaVu Serif"
+        ];
+        emoji = [
+          "Noto Color Emoji"
+          "Symbola"
+        ];
       };
     };
   };
