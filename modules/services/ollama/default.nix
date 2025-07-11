7946d333b739ee308ddf32f389df65951d901d64
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-11 13:50:43 -0400

    temporary searxng instance for use with ollama, will probably break it out into another service at some point

--- modules/services/ollama/default.nix
+++ modules/services/ollama/default.nix
@@ -9,11 +9,31 @@
     acceleration = "rocm";
   };
 
-  networking.firewall.allowedTCPPorts = [3000];
-
-  services.nextjs-ollama-llm-ui = {
+  services.open-webui = {
     enable = true;
     port = 3000;
-    hostname = "0.0.0.0";
+    host = "0.0.0.0";
+    openFirewall = true;
+  };
+
+  services.searx = {
+    enable = true;
+    redisCreateLocally = true;
+    settings = {
+      server.secret_key = "temptestsecret";
+      server.port = 3001;
+      server.bind_address = "0.0.0.0";
+      search.formats = [
+        "html"
+        "json"
+      ];
+    };
+    limiterSettings = {
+      botdetection = {
+        ip_limit.link_token = false;
+        ip_lists.block_ip = [];
+        ip_lists.pass_ip = [];
+      };
+    };
   };
 }
