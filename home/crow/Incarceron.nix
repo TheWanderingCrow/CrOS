0c86ef6243b8a733bb9e5a28c2b13e8850e9fcda
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-14 11:02:41 -0400

    monitors for Incarceron, need to update niri output generation and add swww

--- home/crow/Incarceron.nix
+++ home/crow/Incarceron.nix
@@ -1,8 +1,19 @@
-{...}: {
+{lib, ...}: {
   imports = [
     common/core
     common/optional/desktops/niri.nix
     common/optional/browsers/firefox.nix
     common/optional/comms
+  ];
+
+  monitors = [
+    {
+      name = "eDP-1";
+      primary = true;
+      width = 2256;
+      height = 1504;
+      refreshRate = 60;
+      background = lib.custom.relativeToRoot "assets/wallpapers/kali_lol.jpg";
+    }
   ];
 }
