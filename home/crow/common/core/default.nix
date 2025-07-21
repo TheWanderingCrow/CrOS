ca71898f7896e968c14c296fa221723e763da908
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-21 16:06:59 -0400

    custom cursor

--- home/crow/common/core/default.nix
+++ home/crow/common/core/default.nix
@@ -44,4 +44,11 @@ in {
       EDITOR = "nvim";
     };
   };
+
+  home.pointerCursor = {
+    name = "phinger-cursors-dark";
+    package = pkgs.phinger-cursors;
+    size = 32;
+    gtk.enable = true;
+  };
 }
