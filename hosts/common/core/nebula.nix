ea9f5af1d7eb6bdd63eafb36c310cabb064dc285
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-24 10:14:30 -0400

    build out mesh some

--- hosts/common/core/nebula.nix
+++ hosts/common/core/nebula.nix
@@ -10,4 +10,10 @@ in {
     if !builtins.hasAttr "${config.hostSpec.hostName}" s.hosts
     then [''Hey you don't have a nebula config for this host, you should fix this ASAP so you can be connected to the mesh. If you don't know how to do this then contact your admin'']
     else [];
+
+  services.nebula.networks.wce = lib.mkIf builtins.hasAttr "${config.hostSpec.hostName}" s.hosts {
+    inherit (s) ca;
+    inherit (s.hosts.${config.hostSpec.hostName}) key cert;
+    enable = true;
+  };
 }
