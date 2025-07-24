6c4a91c31de75fadcd5b23fbd42b76de76480fd7
TheWanderingCrow <contact@wanderingcrow.net>
2025-07-24 10:12:31 -0400

    build out mesh some

--- hosts/common/core/nebula.nix
+++ hosts/common/core/nebula.nix
@@ -2,18 +2,12 @@
   config,
   lib,
   inputs,
-  hostSpec,
   ...
 }: let
   s = inputs.nix-secrets.network.mesh;
-in
-  if builtins.hasAttr "${hostSpec.hostName}" s.hosts
-  then lib.warn "Hey you don't have a nebula host config for this host, we'll still build but you should fix this ASAP since you won't be inside the mesh. If you don't know how to do this please talk to your computer administrator (me haha)"
-  else {
-    services.nebula.networks.wce = {
-      inherit (s) ca;
-      inherit (s.hostSpec.hostName) cert key;
-      enable = true;
-      isLighthouse = lib.mkDefault false;
-    };
-  }
+in {
+  warnings =
+    if !builtins.hasAttr "${config.hostSpec.hostName}" s.hosts
+    then [''Hey you don't have a nebula config for this host, you should fix this ASAP so you can be connected to the mesh. If you don't know how to do this then contact your admin'']
+    else [];
+}
