67068ec9d4bc6e16688d861d31ff7ff9185d4cf9
TheWanderingCrow <contact@wanderingcrow.net>
2025-08-07 11:32:24 -0400

    nebula

--- hosts/common/core/nebula.nix
+++ hosts/common/core/nebula.nix
@@ -11,9 +11,11 @@ in {
     then [''Hey you don't have a nebula config for this host, you should fix this ASAP so you can be connected to the mesh. If you don't know how to do this then contact your admin'']
     else [];
 
+  sops.secrets."keys/nebula" = lib.mkIf (builtins.hasAttr "${config.hostSpec.hostName}" s.hosts) {};
   services.nebula.networks.wce = lib.mkIf (builtins.hasAttr "${config.hostSpec.hostName}" s.hosts) {
     inherit (s) ca lighthouses staticHostMap;
-    inherit (s.hosts.${config.hostSpec.hostName}) key cert isLighthouse firewall;
+    inherit (s.hosts.${config.hostSpec.hostName}) cert isLighthouse firewall;
+    key = config.sops.secrets."keys/nebula".path;
     enable = true;
   };
 }
