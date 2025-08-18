c84156748be50f6f13e43a95f0a0ed02d5b60ab9
TheWanderingCrow <contact@wanderingcrow.net>
2025-08-18 11:03:53 -0400

    podman

--- hosts/common/optional/podman.nix
+++ hosts/common/optional/podman.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
   virtualisation.podman = {
     enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+    extraPackages = with pkgs; [
+      podman-compose
+    ];
   };
 }
