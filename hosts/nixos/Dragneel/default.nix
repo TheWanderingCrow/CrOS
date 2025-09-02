08a18d473f3b1211790f7f6d34b189a94568b72d
TheWanderingCrow <contact@wanderingcrow.net>
2025-09-02 12:06:21 -0400

    wayland for drag

--- hosts/nixos/Dragneel/default.nix
+++ hosts/nixos/Dragneel/default.nix
@@ -47,7 +47,10 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
   services.desktopManager.plasma6.enable = true;
 
   networking = {
