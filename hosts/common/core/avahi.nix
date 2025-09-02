59a77075881442170c546828c8ac08fabb5f3442
TheWanderingCrow <contact@wanderingcrow.net>
2025-09-02 09:28:57 -0400

    avahi

--- hosts/common/core/avahi.nix
+++ hosts/common/core/avahi.nix
@@ -5,7 +5,11 @@
     openFirewall = true;
     publish = {
       enable = true;
+      addresses = true;
       domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
     };
   };
 }
