704f8de4e586895bd5cdc285a18ea9f9d3fa8a5f
TheWanderingCrow <contact@wanderingcrow.net>
2025-09-09 11:25:31 -0400

    working on conversion

--- hosts/nixos/HandlerOne/default.nix
+++ hosts/nixos/HandlerOne/default.nix
@@ -84,59 +84,59 @@
   };
 
   #FIXME(TODO) Migrate this into another file, probably a module
-  sops = {
-    secrets = {
-      "aws/access_key" = {};
-      "aws/secret_key" = {};
-      "aws/region" = {};
-    };
-    templates = {
-      "aws_shared_credentials".content = ''
-        [default]
-        aws_access_key_id=${config.sops.placeholder."aws/access_key"}
-        aws_secret_access_key=${config.sops.placeholder."aws/secret_key"}
-      '';
-      "aws_env".content = ''
-        AWS_REGION=${config.sops.placeholder."aws/region"}
-      '';
-    };
-  };
+  #sops = {
+  #  secrets = {
+  #    "aws/access_key" = {};
+  #    "aws/secret_key" = {};
+  #    "aws/region" = {};
+  #  };
+  #  templates = {
+  #    "aws_shared_credentials".content = ''
+  #      [default]
+  #      aws_access_key_id=${config.sops.placeholder."aws/access_key"}
+  #      aws_secret_access_key=${config.sops.placeholder."aws/secret_key"}
+  #    '';
+  #    "aws_env".content = ''
+  #      AWS_REGION=${config.sops.placeholder."aws/region"}
+  #    '';
+  #  };
+  #};
 
-  security.acme = {
-    acceptTerms = true;
-    defaults = {
-      email = "infrastructure@wanderingcrow.net";
-      group = config.services.nginx.group;
-      dnsProvider = "route53";
-      credentialFiles = {
-        "AWS_SHARED_CREDENTIALS_FILE" = config.sops.templates."aws_shared_credentials".path;
-      };
-      environmentFile = config.sops.templates."aws_env".path;
-    };
-    certs = {
-      "wanderingcrow.net" = {};
-      "umami.wanderingcrow.net" = {};
-      "garage.wanderingcrow.net" = {};
-      "bar.wanderingcrow.net" = {};
-      "home.wanderingcrow.net" = {};
-      "homebox.wanderingcrow.net" = {};
-      "cache.wanderingcrow.net" = {};
-      "openhab.wanderingcrow.net" = {};
-      "frigate.wanderingcrow.net" = {};
-      "notes.wanderingcrow.net" = {};
-      "grocy.wanderingcrow.net" = {};
-      "barcodebuddy.grocy.wanderingcrow.net" = {};
-      "budget.wanderingcrow.net" = {};
-      "matrix.wanderingcrow.net" = {};
-      "ta.wanderingcrow.net" = {};
-      "chat.wanderingcrow.net" = {};
-      "netbox.wanderingcrow.net" = {};
-      "notify.wanderingcrow.net" = {};
-      "logs.wanderingcrow.net" = {};
-      "psychal.link" = {};
-      # Sites I host for someone else
-      "swgalaxyproject.com" = {};
-      "nnsbluegrass.com" = {};
-    };
-  };
+  #security.acme = {
+  #  acceptTerms = true;
+  #  defaults = {
+  #    email = "infrastructure@wanderingcrow.net";
+  #    group = config.services.nginx.group;
+  #    dnsProvider = "route53";
+  #    credentialFiles = {
+  #      "AWS_SHARED_CREDENTIALS_FILE" = config.sops.templates."aws_shared_credentials".path;
+  #    };
+  #    environmentFile = config.sops.templates."aws_env".path;
+  #  };
+  #  certs = {
+  #    "wanderingcrow.net" = {};
+  #    "umami.wanderingcrow.net" = {};
+  #    "garage.wanderingcrow.net" = {};
+  #    "bar.wanderingcrow.net" = {};
+  #    "home.wanderingcrow.net" = {};
+  #    "homebox.wanderingcrow.net" = {};
+  #    "cache.wanderingcrow.net" = {};
+  #    "openhab.wanderingcrow.net" = {};
+  #    "frigate.wanderingcrow.net" = {};
+  #    "notes.wanderingcrow.net" = {};
+  #    "grocy.wanderingcrow.net" = {};
+  #    "barcodebuddy.grocy.wanderingcrow.net" = {};
+  #    "budget.wanderingcrow.net" = {};
+  #    "matrix.wanderingcrow.net" = {};
+  #    "ta.wanderingcrow.net" = {};
+  #    "chat.wanderingcrow.net" = {};
+  #    "netbox.wanderingcrow.net" = {};
+  #    "notify.wanderingcrow.net" = {};
+  #    "logs.wanderingcrow.net" = {};
+  #    "psychal.link" = {};
+  #    # Sites I host for someone else
+  #    "swgalaxyproject.com" = {};
+  #    "nnsbluegrass.com" = {};
+  #  };
+  #};
 }
